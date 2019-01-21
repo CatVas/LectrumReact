1cea9eb996c079d972357abfcbcdc94934ef08ee
Vasyl Samusiev <catvaas@googlemail.com>
2019-01-21 22:43:05 +0200

    l4: Pass data into the StatusBar component through context

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,4 +1,5 @@
 import { Feed } from 'components';
+import { Provider } from 'components/HOC/withProfile';
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import avatar from 'theme/assets/lisa';
@@ -12,6 +13,10 @@ const options = {
 @hot(module)
 export default class App extends Component {
     render() {
-        return <Feed { ...options } />;
+        return (
+            <Provider value = { options }>
+                <Feed />
+            </Provider>
+        );
     }
 }
