106e92925465e520c9e5b05a86b32aea5f3be633
Vasyl Samusiev <catvaas@googlemail.com>
2019-01-29 23:11:47 +0200

    react-online-lesson-6: Removing post. Post pending status.

--- source/components/Post/Post.js
+++ source/components/Post/Post.js
@@ -1,22 +1,39 @@
 
+import cn from 'classnames';
 import { Like } from 'components';
 import { Consumer } from 'components/HOC/withProfile';
 import moment from 'moment';
-import { array, func, number, string } from 'prop-types';
+import { array, bool, func, number, string } from 'prop-types';
 import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
     static propTypes = {
-        comment:   string.isRequired,
-        created:   number.isRequired,
-        id:        string.isRequired,
-        likes:     array.isRequired,
-        _likePost: func.isRequired,
+        comment:     string.isRequired,
+        created:     number.isRequired,
+        id:          string.isRequired,
+        likes:       array.isRequired,
+        pending:     bool,
+        _removePost: func.isRequired,
+        _likePost:   func.isRequired,
     };
 
+    constructor () {
+        super();
+
+        this._handlePostRemove = this._handlePostRemove.bind(this);
+    }
+
+    _handlePostRemove () {
+        const { _removePost, id, pending } = this.props;
+
+        if (!pending) {
+            _removePost(id);
+        }
+    }
+
     render() {
-        const { comment, created, id, likes, _likePost } = this.props;
+        const { comment, created, id, likes, pending, _likePost } = this.props;
 
         return (
             <Consumer>
@@ -28,8 +45,13 @@ export default class Post extends Component {
                     } = context;
 
                     return (
-                        <section className = { Styles.post }>
-                            <span className = { Styles.cross } />
+                        <section className = { cn(Styles.post, {
+                            [ Styles.pending ]: pending,
+                        }) }>
+                            <span
+                                className = { Styles.cross }
+                                onClick = { this._handlePostRemove }
+                            />
                             <img src = { avatar } />
                             <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
                             <time>
