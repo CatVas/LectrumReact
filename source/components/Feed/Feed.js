1223a345948fa861e098802517e22e41b83f6f6c
Vasyl Samusiev <catvaas@googlemail.com>
2019-01-29 22:41:59 +0200

    l6: Handling user events (#7)

--- source/components/Feed/Feed.js
+++ source/components/Feed/Feed.js
@@ -1,8 +1,17 @@
 import { Composer, Post, Spinner, StatusBar } from 'components';
+import { delay, getUniqueID } from 'instruments';
+import moment from 'moment';
 import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    constructor () {
+        super();
+        this._createPost = this._createPost.bind(this);
+        this._likePost = this._likePost.bind(this);
+        this._setPostFetchingState = this._setPostFetchingState.bind(this);
+    }
+
     state = {
         isPending: false,
         posts:     [
@@ -10,20 +19,74 @@ export default class Feed extends Component {
                 comment: 'How are you?',
                 created: 1526825076849,
                 id:      '123',
+                likes:   [],
             },
             {
                 comment: 'What\'s up?',
                 created: 1526825079999,
                 id:      '456',
+                likes:   [],
             },
         ],
     };
+
+    async _createPost (comment) {
+        const post = {
+            comment,
+            created: moment().utc()
+                .unix(),
+            id:    getUniqueID(),
+            likes: [],
+        };
+
+        this._setPostFetchingState(true);
+        await delay(1200);
+        this.setState(({ posts }) => ({
+            isPending: false,
+            posts:     [ post, ...posts ],
+        }));
+    }
+
+    async _likePost (id) {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+        const { posts } = this.state;
+
+        this._setPostFetchingState(true);
+        await delay(1200);
+
+        const newPosts = posts.map((post) => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            firstName: currentUserFirstName,
+                            id:        getUniqueID(),
+                            lastName:  currentUserLastName,
+                        },
+                    ],
+                };
+            }
+
+            return post;
+        });
+
+        this.setState({
+            isPending: false,
+            posts:     newPosts,
+        });
+    }
+
+    _setPostFetchingState (isPending) {
+        this.setState({ isPending });
+    }
 
     render() {
         const { isPending, posts } = this.state;
 
         const postsJSX = posts.map((post) => (
             <Post
+                _likePost = { this._likePost }
                 key = { post.id }
                 { ...post }
             />
@@ -33,7 +96,7 @@ export default class Feed extends Component {
             <section className = { Styles.feed }>
                 <Spinner isSpinning = { isPending } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 {postsJSX}
             </section>
         );
