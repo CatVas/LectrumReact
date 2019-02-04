79ffd68480c063121d19a26de867cb9c309bc9e4
Vasyl Samusiev <catvaas@googlemail.com>
2019-02-04 20:44:24 +0200

    react-online-lesson-7: Post and StatusBar wrapped in HOC (#10)

--- source/components/Post/Post.js
+++ source/components/Post/Post.js
@@ -1,13 +1,13 @@
 
 import cn from 'classnames';
 import { Like } from 'components';
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 import moment from 'moment';
 import { array, bool, func, number, string } from 'prop-types';
 import React, { Component } from 'react';
 import Styles from './styles.m.css';
 
-export default class Post extends Component {
+class Post extends Component {
     static propTypes = {
         comment:     string.isRequired,
         created:     number.isRequired,
@@ -26,43 +26,43 @@ export default class Post extends Component {
         }
     }
 
-    render() {
-        const { comment, created, id, likes, pending, _likePost } = this.props;
+    render () {
+        const {
+            avatar,
+            comment,
+            created,
+            currentUserFirstName,
+            currentUserLastName,
+            id,
+            likes,
+            pending,
+            _likePost,
+        } = this.props;
 
         return (
-            <Consumer>
-                {(context) => {
-                    const {
-                        avatar,
-                        currentUserFirstName,
-                        currentUserLastName,
-                    } = context;
-
-                    return (
-                        <section className = { cn(Styles.post, {
-                            [ Styles.pending ]: pending,
-                        }) }>
-                            <span
-                                className = { Styles.cross }
-                                onClick = { this._handlePostRemove }
-                            />
-                            <img src = { avatar } />
-                            <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
-                            <time>
-                                {moment.unix(created).format('MMMM D h:mm:ss a')}
-                            </time>
-                            <p>
-                                {comment}
-                            </p>
-                            <Like
-                                _likePost = { _likePost }
-                                id = { id }
-                                likes = { likes }
-                            />
-                        </section>
-                    );
-                }}
-            </Consumer>
+            <section className = { cn(Styles.post, {
+                [ Styles.pending ]: pending,
+            }) }>
+                <span
+                    className = { Styles.cross }
+                    onClick = { this._handlePostRemove }
+                />
+                <img src = { avatar } />
+                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
+                <time>
+                    {moment.unix(created).format('MMMM D h:mm:ss a')}
+                </time>
+                <p>
+                    {comment}
+                </p>
+                <Like
+                    _likePost = { _likePost }
+                    id = { id }
+                    likes = { likes }
+                />
+            </section>
         );
     }
 }
+
+export default withProfile(Post);
