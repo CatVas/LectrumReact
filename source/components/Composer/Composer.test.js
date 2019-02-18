8a429df1b7a79e76b491782b6f0a80cab38fa20e
Vasyl Samusiev <catvaas@googlemail.com>
2019-02-18 21:13:03 +0200

    react-online-lesson-12: Tests added.

--- source/components/Composer/Composer.test.js
+++ source/components/Composer/Composer.test.js
@@ -8,11 +8,15 @@ const initialState = {
     comment: '',
 };
 const props = {
-    _createPost: jest.fn(),
+    _createPost:          jest.fn(),
+    avatar:               'test avatar',
+    currentUserFirstName: 'test user name',
 };
 const result = mount(<Composer { ...props } />);
 const _handleFormSubmitSpy = jest.spyOn(result.instance(), '_handleFormSubmit');
 const _submitCommentSpy = jest.spyOn(result.instance(), '_submitComment');
+const _submitOnEnter = jest.spyOn(result.instance(), '_submitOnEnter');
+const _updateComment = jest.spyOn(result.instance(), '_updateComment');
 const updatedState = {
     comment,
 };
@@ -26,6 +30,11 @@ describe('component Composer', () => {
         expect(result.find('img')).toHaveLength(1);
     });
 
+    test('img should have src attribute equal to avatar', () => {
+        expect(result.find('img').filter(`[src="${props.avatar}"]`))
+            .toHaveLength(1);
+    });
+
     test('should have 1 input element', () => {
         expect(result.find('input')).toHaveLength(1);
     });
@@ -36,6 +45,13 @@ describe('component Composer', () => {
 
     test('should have 1 textarea element', () => {
         expect(result.find('textarea')).toHaveLength(1);
+    });
+
+    test('should have placeholder with currentUserFirstName', () => {
+        const textareaTest = result
+            .find('textarea')
+            .filter(`[placeholder="What do u think, ${props.currentUserFirstName}?"]`);
+        expect(textareaTest).toHaveLength(1);
     });
 
     test('should have valid initial state', () => {
@@ -64,6 +80,10 @@ describe('component Composer', () => {
         expect(result.state()).toEqual(updatedState);
     });
 
+    test('_updateComment shold be called once', () => {
+        expect(_updateComment).toHaveBeenCalledTimes(1);
+    });
+
     test('should handle form submit event', () => {
         result.find('form').simulate('submit');
         expect(result.state()).toEqual(initialState);
@@ -76,5 +96,20 @@ describe('component Composer', () => {
     test('_handleFormSubmitSpy & _submitCommentSpy should be called once', () => {
         expect(_handleFormSubmitSpy).toHaveBeenCalledTimes(1);
         expect(_submitCommentSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('should handle textarea onKeyPress event', () => {
+        result.find('textarea').simulate('keyPress', {
+            key:            'Enter',
+            preventDefault: jest.fn(),
+        });
+        expect(_submitOnEnter).toHaveBeenCalledTimes(1);
+    });
+
+    test('textatea should react on "Enter" key only', () => {
+        result.find('textarea').simulate('change', {
+            key: 'Lalala',
+        });
+        expect(_submitOnEnter).toHaveBeenCalledTimes(1);
     });
 });
